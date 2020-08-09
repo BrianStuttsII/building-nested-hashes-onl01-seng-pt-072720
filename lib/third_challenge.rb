9b83d12776b458141c3392fb28fe591f0315e848
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-09 21:29:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,15 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => { :name "Lord Montague",
+      :patriarch => { name: "Lord Montague",
       :age "53"
         },
      
-      :matriarch => { :name "Lady Montague", 
+      :matriarch => { name: "Lady Montague", 
       :age "54"
         },
         
-      :hero => { :name "Romeo", :age "15", :status "alive"
+      :hero => { name: "Romeo", :age "15", :status "alive"
       },
       
       :hero_friends => []
