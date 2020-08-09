5c01f9a143a91cbb7acac8dc8ff48bd457253bb1
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-09 21:38:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,9 +12,19 @@ def third_challenge
       :hero => { name: "Romeo", age: "15", status: "alive"
       },
       
-      :hero_friends => []
+      :hero_friends => [
+       { name: "Benvolio"
+      age: "17"
+      attitude:"worried"
+      }
+      
+      { name:"Mercutio"
+      age:"18"
+      attitude:"hot-headed"
+      ]
    }, 
  
+   
    :capulet => {
       :patriarch => {
        
@@ -25,7 +35,18 @@ def third_challenge
       :heroine => { name: "Juliet", age: "15", status: "alive" 
        
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        { name:"Steven"
+        age:"30"
+        attitude:"confused"
+        }
+        
+        {name:"Nurse"
+        age:"44"
+        attitude:"worried"
+        }
+        
+        ]
    }
   }
 
