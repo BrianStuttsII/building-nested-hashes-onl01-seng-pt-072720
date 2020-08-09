b8ab9c4ac3a4bae520aab3316c183b6b4ccf7178
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-09 21:45:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,15 +13,7 @@ def third_challenge
       },
       
       :hero_friends => [
-       { name: "Benvolio",
-      age: "17",
-      attitude:"worried"
-      },
-      
-      { name:"Mercutio",
-      age:"18",
-      attitude:"hot-headed" }
-      
+       
       ]
    }, 
  
@@ -37,15 +29,7 @@ def third_challenge
        
         },
       :heroine_friends => [
-        { name:"Steven",
-        age:"30",
-        attitude:"confused"
-        }, 
-        
-        {name:"Nurse",
-        age:"44",
-        attitude:"worried"
-        }
+       
         
         ]
    }
